a0b29f1c7f376bde1d422720e53c6ae478e986e0
jasonshamayev <60237556+jasonshamayev@users.noreply.github.com>
2020-11-23 21:56:56 -0600

    Registration

--- src/main/java/com/example/loginregistration/RegisterActivity.java
+++ src/main/java/com/example/loginregistration/RegisterActivity.java
@@ -2,13 +2,36 @@ package com.example.loginregistration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextConfirmPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mTextUsername = (EditText) findViewById(R.id.username);
+        mTextPassword = (EditText) findViewById(R.id.password);
+        mTextConfirmPassword = (EditText) findViewById(R.id.rePassword);
+        mButtonRegister = (Button) findViewById(R.id.login);
+        mTextViewLogin = (TextView) findViewById(R.id.register);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent LoginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(LoginIntent);
+            }
+        });
     }
 }
