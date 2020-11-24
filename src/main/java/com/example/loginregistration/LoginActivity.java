99991a30b6ae848af1b8745a2d675a3c36649d67
jasonshamayev <60237556+jasonshamayev@users.noreply.github.com>
2020-11-24 16:44:46 -0600

    Complete Registration

--- src/main/java/com/example/loginregistration/LoginActivity.java
+++ src/main/java/com/example/loginregistration/LoginActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,12 +17,14 @@ public class LoginActivity extends AppCompatActivity {
     EditText mTextPassword;
     Button mButtonLogin;
     TextView mTextViewRegister;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        db = new DatabaseHelper(this);
         mTextUsername = (EditText)findViewById(R.id.username);
         mTextPassword = (EditText)findViewById(R.id.password);
         mButtonLogin = (Button) findViewById(R.id.login);
@@ -33,6 +37,20 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pass = mTextPassword.getText().toString().trim();
+                Boolean res = db.checkUserExists(user, pass);
+                if (res == true) {
+                    Toast.makeText(LoginActivity.this, "Successfully Logged In", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+                }
 
+            }
+        });
     }
 }
--- src/main/java/com/example/loginregistration/RegisterActivity.java
+++ src/main/java/com/example/loginregistration/RegisterActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    DatabaseHelper db;
     EditText mTextUsername;
     EditText mTextPassword;
     EditText mTextConfirmPassword;
@@ -21,16 +24,43 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mTextUsername = (EditText) findViewById(R.id.username);
-        mTextPassword = (EditText) findViewById(R.id.password);
-        mTextConfirmPassword = (EditText) findViewById(R.id.rePassword);
-        mButtonRegister = (Button) findViewById(R.id.login);
-        mTextViewLogin = (TextView) findViewById(R.id.register);
+        db = new DatabaseHelper(this);
+        mTextUsername = (EditText) findViewById(R.id.edittext_username);
+        mTextPassword = (EditText) findViewById(R.id.edittext_password);
+        mTextConfirmPassword = (EditText) findViewById(R.id.edittext_rePassword);
+        mButtonRegister = (Button) findViewById(R.id.buttonRegister);
+        mTextViewLogin = (TextView) findViewById(R.id.textView_login);
         mTextViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent LoginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(LoginIntent);
+            }
+        });
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pass = mTextPassword.getText().toString().trim();
+                String confirmPass = mTextConfirmPassword.getText().toString().trim();
+
+                if(pass.equals(confirmPass))
+                {
+                    long val = db.addUser(user,pass);
+                    if(val > 0) {
+                        Toast.makeText(RegisterActivity.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                        Intent moveToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(moveToLogin);
+                    }
+                    else{
+                        Toast.makeText(RegisterActivity.this, "Error Registering. Please Try Again.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
